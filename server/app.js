ae56017f74d66a0fb95360f01608135b69878834
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-10 07:06:40 +1200

    Added GET route for /planets/id

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // general modules
-const express = require('express') 
-const cors = require('cors')
+import express from 'express'
+import cors from 'cors'
+import fetch from 'node-fetch'
 
 const app = express()
 
@@ -13,8 +14,26 @@ app.use(express.json())
 // parse incoming requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/api/test", (req, res) => {
-    return res.status(200).send({msg: "Hello world~"})
+const BASE_URL = "https://swapi.dev/api/"
+
+app.get("/api/planets/:id", (req, res) => {
+    const planetId = req.params.id
+
+    fetch(`${BASE_URL}/planets/${planetId}`)
+        .then(res => res.json())
+        .then(result => {
+            if (!("name" in result)) {
+                return res.status(400).json({
+                    error: "Please provide a valid planet id."
+                })
+            }
+            return res.status(200).json({
+                name: result.name
+            })
+        })
+        .catch(e => {
+            return res.status(500).json({error: "Internal Server Error."})
+        })
 })
 
-module.exports = app
+export default app

7905e8053634492410813ff27f95a3ed9feac473
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-11 21:34:03 +1200

    Fixed UI bug. Now person cards appear in a column on small devices.

--- client/src/components/CharacterCard/index.js
+++ client/src/components/CharacterCard/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import styles from "./character-card.module.css"
 const { container, text } = styles
 
-export default function CharacterCard({ name, id }) {
+export default function CharacterCard({ name }) {
     return (
         <Link to={`/characters/${name}`} style={{textDecoration: "none"}}>
             <div className={container}>

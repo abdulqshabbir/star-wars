142111f0adb48149b89923b3529b2ab0ef062f3e
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-11 21:55:35 +1200

    completed styling of submit button on search bar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,12 @@
 import { GiMagicPortal } from "react-icons/gi"
 import { BsSearch } from "react-icons/bs"
-import { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom"
 
 import styles from "./header.module.css"
-const { container, logoContainer, logoText, searchBarContainer, searchText } = styles
+const { container, logoContainer, logoText, searchBarContainer, searchText, searchButton } = styles
 
 export default function Header() {
-    const [searchString, setSearchString] = useState("")
-    const inputRef = useRef()
-
-    useEffect(() => inputRef.current.focus(), [])
-
     return (
         <header className={container}>
             <Link to="/" style={{textDecoration: "none"}} className={logoContainer}>
@@ -20,14 +15,26 @@ export default function Header() {
             </Link>
             <div className={searchBarContainer}>
                 <BsSearch size="25px"/>
-                <input
-                    ref={inputRef}
-                    className={searchText}
-                    type="text"
-                    value={searchString}
-                    onChange={e => setSearchString(e.target.value)}
-                />
+                <SearchBar /> 
             </div>
         </header>
+    )
+}
+
+function SearchBar() {
+    const [searchString, setSearchString] = useState("")
+    const inputRef = useRef()
+    useEffect(() => inputRef.current.focus(), [])
+    return(
+        <React.Fragment>
+            <input
+                ref={inputRef}
+                className={searchText}
+                type="text"
+                value={searchString}
+                onChange={e => setSearchString(e.target.value)}
+            />
+            <button className={searchButton}>Submit</button>
+        </React.Fragment>
     )
 }

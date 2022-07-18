945f87f9394535f769fd17417629ad0d075063b3
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-18 18:41:51 +1200

    Added images for all Star Wars characters.

--- client/src/components/CharacterCard/index.js
+++ client/src/components/CharacterCard/index.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import styles from "./character-card.module.css"
-const { container, text } = styles
+const { container, text, image } = styles
 
 export default function CharacterCard({ name }) {
+                console.log(name)
     return (
         <Link to={`/characters/${name}`} style={{textDecoration: "none", color: 'white'}}>
             <div className={container}>
                 <p className={text}>{name}</p>
+                <img className={image} src={`${process.env.PUBLIC_URL}/${name}.jpg`} height={'200px'} alt="Sorry no image available" />
             </div>
         </Link>
     )
